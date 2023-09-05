471b14edcafd02c9d39f8ab8609b245efce48cdf
root <rozenhertfatima>
2023-09-05 02:50:32 -0700

    0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:     
+    
+     print(number, f"is possitive") 
+elif number == 0:   
+     print(number,f"is ziro",) 
+else:
+    print(number, f"is negative") 
+   
+
