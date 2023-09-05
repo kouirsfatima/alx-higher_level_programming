77713f0d22a6fabc43c7aae93ed29a27479417a3
root <rozenhertfatima>
2023-09-05 07:05:11 -0700

    modified:   5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i == 100:
-        print("\n")
-    print("{:02d}".format(i), end=", ")
+    if i == 99:
+        ptr = "\n"
+    else:
+        ptr = ", "
+    print("{:02d}".format(i), end=ptr)
