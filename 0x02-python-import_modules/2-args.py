a94ac20e983961d396c11278c30add7072af242b
kouirsfatima <fatimaberline535@gmail.com>
2023-09-07 14:15:50 +0100

    3-infinite_add.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/bin/python3
-import sys
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
+    from sys import argv
      i = len(sys.argv) - 1
 if i == 0:        
     print("{} arguments.".format(i))
@@ -9,8 +9,9 @@ elif i == 1:
     print("{} argument:".format(i))
 else:
     print("{} arguments:".format(i))
-if i >= 1 or i != 0:
+if i >= 1:
+    i == 0
     for arg in sys.argv:
         if i != 0:
             print("{}: {}".format(i, arg))
-        i += 1
+            i += 1
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for i in range(1, len(argv)):
+        add += int(argv[i])
+    print("{:d}".format(add))
