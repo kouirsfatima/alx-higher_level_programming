cdde917cf87bd6cac4628543fce920462794026c
kouirsfatima <fatimaberline535@gmail.com>
2023-09-13 11:02:10 +0100

    all file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
+       my_list = list(map(lambda x: replace if x == search else x, my_list))
     return(my_list)
