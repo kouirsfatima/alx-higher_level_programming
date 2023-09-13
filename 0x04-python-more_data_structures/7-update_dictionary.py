2a297a46b6585b008d3fbe921501d5da01e99549
kouirsfatima <fatimaberline535@gmail.com>
2023-09-13 14:38:13 +0100

    all file

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    sorted_keys = sorted(a_dictionary.keys())
+    for i in sorted_keys:
+        return (a_dictionary)
