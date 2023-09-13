8bf87e263831aa6bd8c3bffddb2a47bc29292d3e
kouirsfatima <fatimaberline535@gmail.com>
2023-09-13 11:57:48 +0100

    all file

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for i in range(len(a_dictionary)):
-        lenght = len(a_dictionary)
-        return(lenght)
+    lista = list(a_dictionary.keys())
+    return len(lista)
