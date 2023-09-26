f013ddb57fd02dcdc8edaced0cea8aaf80a2c88d
kouirsfatima <fatimaberline535@gmail.com>
2023-09-26 15:13:40 +0100

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Create a Square class with a private attribute size"""
 
+
 class Square:
     """Initialize the Square class with a private attribute size"""
 
     def __init__(self, size):
         """Initialize a square with the given size.
-        
         Args:
             size (int): The size of the square.
         """
