4a391a5e5313b7119329f06becadd169e5c234b3
kouirsfatima <fatimaberline535@gmail.com>
2023-09-26 19:41:39 +0100

    6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,46 @@
+class Square:
+    """Initialize the Square class with a private attribute size"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 \
+                or not isinstance(value[0], int) or not isinstance(value[1], int) \
+                or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+def my_print(self):
+    if self.__size == 0:
+        print()
+    else:
+        for _ in range(self.position[1]): 
+            print()
+        for _ in range(self.__size):
+            print(" " * self.position[0], end="") 
+            print("#" * self.__size)
