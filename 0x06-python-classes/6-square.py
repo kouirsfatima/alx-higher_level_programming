f7dff74a836c5727b76ce26f61c2e2e73092e39b
kouirsfatima <fatimaberline535@gmail.com>
2023-09-27 10:24:09 +0100

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,32 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+    
+"""Create a Square class with a private attribute size"""
+
 
 class Square:
+    """Initialize the Square class with a private attribute size"""
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
     def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
             raise TypeError("size must be an integer")
