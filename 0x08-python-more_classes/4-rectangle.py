7da0043cc568a1345c41fff3dc77fdc6de60113f
kouirsfatima <fatimaberline535@gmail.com>
2023-10-02 13:58:22 +0100

    3-main.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-"""Empty class Rectangle that defines a Rectangle """
+"""Define a Rectangle class."""
 
 
 class Rectangle:
-    """Empty class Rectangle that defines a Rectangle """
+    """Define a Rectangle class."""
 
     def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Width property getter."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Width property setter."""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Height property getter."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Height property setter."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -34,16 +39,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Return the area of the rectangle."""
         return self.__height * self.__width
 
     def perimeter(self):
+        """Return the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """Return a string representation of the rectangle."""
         if self.__height == 0 or self.__width == 0:
             return ""
-        return "\n".join(['#' * self.__width
-                          for _ in range(self.__height)])
-
+        return "\n".join(['#' * self.__width for _ in range(self.__height)])
