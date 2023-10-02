bb310ec4b543bfb74984ae70aac430d90520ce43
kouirsfatima <fatimaberline535@gmail.com>
2023-10-02 14:21:54 +0100

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -53,7 +53,6 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
         return "\n".join(['#' * self.__width for _ in range(self.__height)])
-    def __repr__(self):
-         return f"Rectangle({self.__width}, {self.__height})"
 
-         
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
