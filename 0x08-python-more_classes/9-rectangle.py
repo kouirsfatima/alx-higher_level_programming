a2200bef25671c2cb97a5a4a147dc296e4e45fa6
kouirsfatima <fatimaberline535@gmail.com>
2023-10-02 16:36:52 +0100

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -28,6 +28,11 @@ class Rectangle:
         else:
             return rect_2
 
+    @classmethod
+    def square(cls, size=0):
+        """Return a new Rectangle if height == width."""
+        return cls(width=size, height=size)
+
     def area(self):
         """Return the area of the rectangle."""
         return self.height * self.width
@@ -51,8 +56,3 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @classmethod
-    def square(cls, size=0):
-        """Return a new Rectangle if height == width."""
-        return cls(width=size, height=size)
