2246a14009b4cd63cb1edda4bf760b28c0de988e
kouirsfatima <fatimaberline535@gmail.com>
2023-10-02 12:20:19 +0100

    3-main.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -44,3 +44,10 @@ class Rectangle:
     def __str__(self):
         return "\n".join(['#' * self.__width
                           for _ in range(self.__height)])
+
+    def my_print(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            print("\n".join(['#' * self.__width
+                             for _ in range(self.__height)]))
