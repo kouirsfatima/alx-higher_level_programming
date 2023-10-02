8afaeb2b34960ab0b465fea9fa0f84c9fbf2bc21
kouirsfatima <fatimaberline535@gmail.com>
2023-10-02 16:46:22 +0100

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
         """Instantiation with optional width and height."""
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
@@ -23,15 +23,12 @@ class Rectangle:
         area_1 = rect_1.area()
         area_2 = rect_2.area()
 
-        if area_1 >= area_2:
-            return rect_1
-        else:
-            return rect_2
+        return rect_1 if area_1 >= area_2 else rect_2
 
     @classmethod
     def square(cls, size=0):
-        """Return a new Rectangle height == width."""
-        return Rectangle(size, size)
+        """Return a new Rectangle with height == width."""
+        return cls(width=size, height=size)
 
     def area(self):
         """Return the area of the rectangle."""
@@ -47,13 +44,12 @@ class Rectangle:
         """Return a string representation of the rectangle."""
         if self.height == 0 or self.width == 0:
             return ""
-        symbol_line = str(Rectangle.print_symbol) * self.width
+        symbol_line = str(self.print_symbol) * self.width
         return "\n".join([symbol_line for _ in range(self.height)])
 
     def __repr__(self):
-        
         return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
