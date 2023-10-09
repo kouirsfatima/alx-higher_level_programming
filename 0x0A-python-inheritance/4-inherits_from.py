8628961bdec0689818f79f0165ab6dfb19f53d5c
kouirsfatima <fatimaberline535@gmail.com>
2023-10-09 16:35:11 +0100

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""returns True if the object is exactly an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class; otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is exactly an instance"""
+    if type(obj) is not a_class:
+        return True
+    else:
+        return False
