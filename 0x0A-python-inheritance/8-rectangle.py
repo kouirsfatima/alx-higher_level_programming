63648ca24908fb83170f0fa530c1b0d5ed957908
kouirsfatima <fatimaberline535@gmail.com>
2023-10-09 18:06:43 +0100

    8-retangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Base geometry class"""
+
+
+class BaseGeometry:
+    """Base class"""
+
+    def area(self):
+        """Method to calculate the area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method to validate an integer value"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """Constructor method for Rectangle"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
