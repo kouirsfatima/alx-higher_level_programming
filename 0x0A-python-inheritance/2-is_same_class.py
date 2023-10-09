7448ca773d60dd3835895d40a63a43c760c82014
kouirsfatima <fatimaberline535@gmail.com>
2023-10-09 14:52:12 +0100

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """returns True if the object is exactly an instance of the specified class
- ; otherwise False"""
+; otherwise False.
+"""
 
 
-class MyClass:
-
-    def is_same_class(obj, a_class):
-        """Check if the object is exactly an instance
-        of the specified class."""
-        if isinstance(obj, a_class):
-            return True
-        else:
-            return False
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
--- 0x0A-python-inheritance/2-main.py
+++ 0x0A-python-inheritance/2-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-is_same_class = __import__('2-is_same_class').MyClass.is_same_class
+is_same_class = __import__('2-is_same_class').is_same_class
 
 a = 1
 if is_same_class(a, int):
@@ -8,3 +8,4 @@ if is_same_class(a, float):
     print("{} is an instance of the class {}".format(a, float.__name__))
 if is_same_class(a, object):
     print("{} is an instance of the class {}".format(a, object.__name__))
+
