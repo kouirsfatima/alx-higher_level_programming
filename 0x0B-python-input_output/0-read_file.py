94cdc9ecc35c90936585673e71e993a339e8477e
kouirsfatima <fatimaberline535@gmail.com>
2023-10-10 14:05:39 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """Function that reads content from a file."""
 
 
-def read_file(filename="my_file_0.txt"):
+def read_file(filename=""):
     """
     Read the contents of a text file and print it.
 
@@ -13,6 +13,6 @@ def read_file(filename="my_file_0.txt"):
         Exception: When the file cannot be opened.
     """
 
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_content = f.read()
+    with open(filename, 'r', encoding="utf-8") as file:
+        read_content = file.read()
         print(read_content, end=" ")
