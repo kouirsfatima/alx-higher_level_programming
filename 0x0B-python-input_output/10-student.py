b22c372f1df6a00f4a2b6aa03353c2a647bc9c54
kouirsfatima <fatimaberline535@gmail.com>
2023-10-10 20:54:49 +0100

    10-main.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """ Class to create student instances """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+
+        if attrs is None:
+            return obj
+
+        if not isinstance(attrs, list):
+            return obj
+
+        result_dict = {}
+        for attr in attrs:
+            if not isinstance(attr, str):
+                return obj
+
+        for attr in attrs:
+            if attr in obj:
+                result_dict[attr] = obj[attr]
+
+        return result_dict
