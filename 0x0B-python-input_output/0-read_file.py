c8e2e871e48a8ff10f97c6096f39b6727cd6603d
kouirsfatima <fatimaberline535@gmail.com>
2023-10-10 13:50:19 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""  function that reads containe the file from a file """
+"""Function that reads content from a file."""
 
 
 def read_file(filename="my_file_0.txt"):
-    """Read the contents of a text file and print it."""
+    """
+    Read the contents of a text file and print it.
+
+    Args:
+        filename (str): The name of the file.
+
+    Raises:
+        Exception: When the file cannot be opened.
+    """
+
     with open(filename, 'r', encoding="utf-8") as file:
-        read_file = file.read()
-        print(read_file)
+        read_content = file.read()
+        print(read_content)
