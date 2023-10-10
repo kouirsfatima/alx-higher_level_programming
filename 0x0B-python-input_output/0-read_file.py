29859b6a639b748af81c0b7ebc51991323389340
kouirsfatima <fatimaberline535@gmail.com>
2023-10-10 13:52:27 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -15,4 +15,4 @@ def read_file(filename="my_file_0.txt"):
 
     with open(filename, 'r', encoding="utf-8") as file:
         read_content = file.read()
-        print(read_content)
+        print(read_content, end=" ")
