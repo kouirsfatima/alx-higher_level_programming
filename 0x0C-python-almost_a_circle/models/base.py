243f55700b0173e8af93c686c8c373ad2f85c63c
kouirsfatima <fatimaberline535@gmail.com>
2023-10-16 17:29:34 +0100

    fix error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-"""
-import json
-import csv
-"""
-import json
-import csv
+"""Defines a base model class"""
 
-"""module Base"""
+
+import json
 
 
 class Base:
-    """This class represent a Base model."""
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base Object.
+        """
+        Initializes a new instance of the Base class.
+
         Args:
-            id (int): The ident of The Base
+            id (int): The identifier for the instance.
         """
         if id is not None:
             self.id = id
@@ -27,67 +29,53 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
-        Returns the JSON string representation of list_dictionaries
+        """Return the JSON serialization of a list of dicts.
 
         Args:
-            list_dictionaries (list): List of dictionaries.
-
-        If list_dictionaries is None or  an empty list return '[]'.
-        Overwrite json string.
+            list_dictionaries (list): A list of dictionaries.
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None:
             return "[]"
-        str_dict = json.dumps(list_dictionaries)
-        return str_dict
+
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """
-        Write the JSON string representation of list_objs to a file.
+        """Write the JSON serialization of a list of objects to a file.
 
         Args:
-            list_objs (list): List of instances to be saved.
-
-        If list_objs is None, save an empty list.
-        Overwrite the file if it already exists.
+            list_objs (list): A list of inherited Base instances.
         """
-        _list = []
-        if list_objs is not None:
-            for obj in list_objs:
-                _list.append(obj.to_dictionary())
+        file1 = cls.__name__ + "_file1.json"
+        if list_objs is None:
+            list_objs = []
+        dict_list = [obj.to_dictionary() for obj in list_objs]
+        json_string = Base.to_json_string(dict_list)
+        with open(file1, 'w') as file:
+            file.write(json_string)
 
-        filename = f"{cls.__name__}.json"
-        json_str = cls.to_json_string(_list)
-
-        with open(filename, "w") as file:
-            file.write(json_str)
-
-    @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string.
         Args:
             json_string (str): string representing a list of dictionaries.
         """
-        if json_string is None or len(json_string) == 0:
+
+        if json_string is None:
             return []
-        return json.loads(json_string)
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """
-        Create an instance with attributes set based on
-            the provided dictionary.
-        Args:
-            **dictionary: a double pointer to a dictionary
+        """Return a class instantied from a dictionary of attributes.
 
-        Return:
-            Instance of the class with attributes set.
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
         """
-        if cls.__name__ == 'Rectangle':
+
+        if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
         else:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
-
