63a732468d12fec58cfbed407237364edec599f1
kouirsfatima <fatimaberline535@gmail>
2023-11-14 20:04:13 +0100

    2-remove_database.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- creates a table called first_table in my current DB
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+-- Prints the full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
