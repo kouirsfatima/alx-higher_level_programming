caf2740bff080385e94b274dd6c50040589afcc6
kouirsfatima <fatimaberline535@gmail>
2023-11-15 15:28:08 +0100

    " 0-privileges.sql"

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create MySQL server user user_0d_1 if it doesn't exist and grant privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
