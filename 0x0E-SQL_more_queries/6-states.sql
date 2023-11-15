4162591974e1900d77f43ad46ac399c627ca5650
kouirsfatima <fatimaberline535@gmail>
2023-11-15 20:11:15 +0100

    " 6-states.sql"

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- Create the table states in the database hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    `id` INT DEFAULT 1 NOT NULL,
+    `id` INT DEFAULT 1 NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL,
     UNIQUE (`id`)
 );
