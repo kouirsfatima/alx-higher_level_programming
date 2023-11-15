b90df50da85dd69dbf71ceb2f64dda9a6b8e5bd4
kouirsfatima <fatimaberline535@gmail>
2023-11-15 20:14:09 +0100

    " 6-states.sql"

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,7 @@
--- Create the database hbtn_0d_usa if it doesn't exist
+-- Creates the table hbtn_0d_usa with table states.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-
--- Create the table states in the database hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.`1`states` (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     PRIMARY KEY(`id`),
-    `id` INT DEFAULT 1 NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL,
-    UNIQUE (`id`)
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
