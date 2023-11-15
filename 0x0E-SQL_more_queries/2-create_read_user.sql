2e6471366b9195e35d1d055319519a5168a55ea0
kouirsfatima <fatimaberline535@gmail>
2023-11-15 19:25:01 +0100

    " 0-privileges.sql "

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Create MySQL server user user_0d_2 if it doesn't exist and grant privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT ALL PRIVILEGES GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';;
-
--- Create database hbtn_0d_2 if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
