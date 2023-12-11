1198553c473ad68b967868d2fee86dbaa4ede90f
kouirsfatima <fatimaberline535@gmail>
2023-12-11 22:07:15 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
+
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+
+if (count === 2)
+  console.log('No argument');
+else if (count === 3)
+  console.log('Argument found');
+else
+  console.log('Arguments found');
