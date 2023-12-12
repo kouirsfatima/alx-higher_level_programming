b103ac1291abb20109ffb18f0adfb1f9b29b1c71
kouirsfatima <fatimaberline535@gmail>
2023-12-12 21:02:27 +0100

    1-0maim.c

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
+class Rectangle {
     constructor(w, h) {
       this.width = w;
-      this.hight = h;
+      this.height = h;
     }
-  };
+  }
+  
+  module.exports = Rectangle;
   
