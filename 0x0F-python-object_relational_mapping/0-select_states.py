5f978e48fa9673152a703dd37a4932f0a3839eb4
kouirsfatima <fatimaberline535@gmail>
2023-12-14 22:32:58 +0100

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""module for states"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    count = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    curs = count.cursor()
+
+    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = curs.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    curs.close()
+    count.close()
