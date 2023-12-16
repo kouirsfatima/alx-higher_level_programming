cbf6587ea628398ac8ec91f2ac996daf7893d181
kouirsfatima <fatimaberline535@gmail>
2023-12-16 12:32:31 +0100

    all file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
         db=argv[3]
     )
 
-    curs = db.cursor()
-    curs.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%' ORDER BY id ASC")
-    query_rows = curs.fetchall()
-
-    for row in query_rows:
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
-    db.close()   
+    cur.close()
+    db.close()
