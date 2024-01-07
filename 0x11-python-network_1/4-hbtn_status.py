1a317ace396f3bf7197a056df1ce73856ca72f2d
kouirsfatima <fatimaberline535@gmail.com>
2024-01-07 23:08:07 +0100

    0-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ requests model
 
 if __name__ == '__main__':
     import requests
-import urllib.request
+
     html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(html.text.__class__))
