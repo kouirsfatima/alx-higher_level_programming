96ddd81561c5148763dea576e84313a82e0467ca
kouirsfatima <fatimaberline535@gmail.com>
2024-01-07 22:13:55 +0100

    0-hbtn_status.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that:
+- sends a request to the URL and displays the value
+"""
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        dc = dict(req.headers).get("X-Request-Id")
+        print(dc)
